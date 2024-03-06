24665cfe5155456c16cc839634d840cae4efa229
Dessian-Guiro <135682316+Dessian-Guiro@users.noreply.github.com>
2024-03-05 23:37:48 -0600

    final commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const pg = require('pg')
 const express = require('express')
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_employees_departements_db')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 app.use(express.json())
 app.use(require('morgan')('dev'))
 // ... your routes go next
@@ -10,7 +10,7 @@ app.use(require('morgan')('dev'))
 app.get('/api/departments', async (req, res, next) => {
     try {
       const SQL = `
-        SELECT * from department
+        SELECT * from departments
       `
       const response = await client.query(SQL)
       res.send(response.rows)
@@ -38,7 +38,7 @@ app.get('/api/departments', async (req, res, next) => {
         VALUES($1, $2)
         RETURNING *
       `
-      const response = await client.query(SQL, [req.body.txt, req.body.category_id])
+      const response = await client.query(SQL, [req.body.name, req.body.category_id])
       res.send(response.rows[0])
     } catch (ex) {
       next(ex)
@@ -53,7 +53,7 @@ app.get('/api/departments', async (req, res, next) => {
         WHERE id=$4 RETURNING *
       `
       const response = await client.query(SQL, [
-        req.body.nmae,
+        req.body.name,
         req.body.ranking,
         req.body.department_id,
         req.params.id
@@ -77,34 +77,38 @@ app.get('/api/departments', async (req, res, next) => {
     }
   });
 // ... next, inside your init function
-app.listen(port, () => console.log(`listening on port ${port}`))
+
 
 
 const init = async () => {
     await client.connect();
     console.log('connected to database')
     let SQL = `DROP TABLE IF EXISTS employees;
-    DROP TABLE IF EXISTS departments;
-    CREATE TABLE departements(
+    DROP TABLE IF EXISTS departments CASCADE;
+    CREATE TABLE departments(
       id SERIAL PRIMARY KEY,
       name VARCHAR(100)
     );
-    CREATE TABLE notes(
+    CREATE TABLE employees(
       id SERIAL PRIMARY KEY,
       created_at TIMESTAMP DEFAULT now(),
       updated_at TIMESTAMP DEFAULT now(),
-      departement_id INTEGER DEFAULT 3 NOT NULL,
+      
       name VARCHAR(255) NOT NULL,
-      departement_id INTEGER REFERENCES departments(id) NOT NULL
+      department_id INTEGER REFERENCES departments(id) NOT NULL
     );`
     await client.query(SQL)
     console.log('tables created')
-    SQL = `INSERT INTO categories(name) VALUES('SQL');
-    INSERT INTO categories(name) VALUES('Express');
-    INSERT INTO categories(name) VALUES('Shopping');
-    INSERT INTO employees(name, ranking, department_id) VALUES('ALI', 5, (SELECT id FROM departments WHERE name='Ali'));
-    INSERT INTO employees(name, ranking, department_id) VALUES('add logging middleware', 5, (SELECT id FROM categories WHERE name='Ali'));
-    INSERT INTO employees((name, ranking, department_id) VALUES('write SQL queries', 4, (SELECT id FROM departments WHERE name='NANCY'));
-    INSERT INTO employees((name, ranking, department_id) VALUES('learn about foreign keys', 4, (SELECT id FROM departments WHERE name='NANCY'));
-    INSERT INTO employees((name, ranking, department_id) VALUES('buy a quart of milk', 2, (SELECT id FROM departments WHERE name='BOB'));`
+    SQL = `INSERT INTO departments(name) VALUES('HR');
+    INSERT INTO departments(name) VALUES('OUTBOUND');
+    INSERT INTO departments(name) VALUES('INBOUND');
+    INSERT INTO employees(name, department_id) VALUES('add logging middleware', (SELECT id FROM departments WHERE name='HR'));
+    INSERT INTO employees(name, department_id) VALUES('John Doe', (SELECT id FROM departments WHERE name='OUTBOUND'));
+    INSERT INTO employees(name, department_id) VALUES('Jane Smith', (SELECT id FROM departments WHERE name='OUTBOUND'));
+    INSERT INTO employees(name, department_id) VALUES('Alice Johnson', (SELECT id FROM departments WHERE name='HR'));
+    INSERT INTO employees(name, department_id) VALUES('Bob Brown', (SELECT id FROM departments WHERE name='INBOUND'))`;
+    await client.query(SQL)
+    app.listen(port, () => console.log(`listening on port ${port}`))
+
 }
+init()
